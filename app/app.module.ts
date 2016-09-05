8b15726375b9de93eb43b19eb367c7a395035c3a
alliearsenault <ajarsenault@techsoft.com>
2016-09-05 16:35:27 -0400

    Broke out hero templates

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./dashboard.component";
 import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail-component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 import { HeroService } from "./hero.service";
 

508c4aceffc0478c5597348af7fe88f551cb4358
Allie Arsenault <alliejarsenault@gmail.com>
2016-09-06 21:39:33 -0400

    added in memory data service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+//Import for loading and configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data-service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from "./dashboard.component";
@@ -15,6 +20,8 @@ import { routing } from './app.routing';
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         routing
     ],
     declarations: [
